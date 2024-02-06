18c53cb7f366e08a4243d6e56f849bc2922b21b0
aausachova <143784011+aausachova@users.noreply.github.com>
2024-02-06 23:00:23 +0300

    task 1

---
+++ T01D01-0-develop/src/quest3.sh
@@ -0,0 +1,8 @@
+chmod +x ai_door_control.sh
+mv door_management_fi door_management_files
+mkdir door_configuration
+mkdir door_logs
+mkdir door_map
+mv *.conf door_configuration
+mv *.log door_logs
+mv door_map_1.1 door_map:
